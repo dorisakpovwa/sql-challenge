a303368910f0136913d264aa1879e0c0c23bc32d
dorisakpovwa <75711579+dorisakpovwa@users.noreply.github.com>
2021-02-26 21:30:06 -0500

    corrected files

--- EmployeeSQL/table_schemata.sql
+++ EmployeeSQL/table_schemata.sql
@@ -1,28 +1,17 @@
-Drop Table Department;
+Drop Table Title cascade;
+CREATE TABLE Title (
+    Title_ID VARCHAR   NOT NULL,
+    Tiltle VARCHAR   NOT NULL,
+    PRIMARY KEY (Title_ID));
+	
+Drop Table Department cascade;
 CREATE TABLE Department (
-    ID VARCHAR   NOT NULL,
+    Dept_ID VARCHAR   NOT NULL,
     Dept_Name VARCHAR   NOT NULL,
-    CONSTRAINT pk_Department PRIMARY KEY (
-        ID
-     )
-);
-Drop Table Emp_Dept;
-CREATE TABLE Emp_Dept (
-    Emp_ID INTEGER   NOT NULL,
-    Department_ID VARCHAR   NOT NULL,
-    CONSTRAINT pk_Emp_Dept PRIMARY KEY (
-        Emp_ID,Department_ID
-     )
-);
-Drop Table Mgr_Dept;
-CREATE TABLE Mgr_Dept (
-    Department_ID VARCHAR   NOT NULL,
-	Emp_ID INTEGER   NOT NULL,
-    CONSTRAINT pk_Mgr_Dept PRIMARY KEY (
-        Emp_ID,Department_ID
-     )
+    PRIMARY KEY (Dept_ID)
 );
 
+Drop Table Employees cascade;
 CREATE TABLE Employees (
     Emp_ID INTEGER   NOT NULL,
     Title VARCHAR   NOT NULL,
@@ -31,25 +20,32 @@ CREATE TABLE Employees (
     Last_Name VARCHAR   NOT NULL,
     SEX VARCHAR   NOT NULL,
     Hire_Date DATE   NOT NULL,
-    CONSTRAINT pk_Employees PRIMARY KEY (
-        Emp_ID
-     )
-);
+    PRIMARY KEY (Emp_ID),
+    FOREIGN KEY (Title) REFERENCES Title (Title_ID));
+	
+Drop Table Emp_Dept cascade;
+CREATE TABLE Emp_Dept (
+    Emp_ID INTEGER   NOT NULL,
+    Dept_ID VARCHAR   NOT NULL,
+    PRIMARY KEY (Emp_ID, Dept_ID),
+	FOREIGN KEY (Emp_ID) REFERENCES Employees (Emp_ID),
+	FOREIGN KEY (Dept_ID) REFERENCES Department (Dept_ID));
+	
 
+Drop Table Mgr_Dept;
+CREATE TABLE Mgr_Dept (
+    Dept_ID VARCHAR   NOT NULL,
+	Emp_ID INTEGER   NOT NULL,
+	PRIMARY KEY (Dept_ID, Emp_ID),
+	FOREIGN KEY (Dept_ID) REFERENCES Department (Dept_ID),
+	FOREIGN KEY (Emp_ID) REFERENCES Employees (Emp_ID));
+
+Drop Table Salaries;	
 CREATE TABLE Salaries (
     Emp_ID INTEGER   NOT NULL,
     Salary INT   NOT NULL,
-    CONSTRAINT pk_Salaries PRIMARY KEY (
-        Emp_ID
-     )
-);
+    PRIMARY KEY (Emp_ID),
+	FOREIGN KEY (Emp_ID) REFERENCES Employees (Emp_ID));
 
-CREATE TABLE Title (
-    Title_ID VARCHAR   NOT NULL,
-    Tiltle VARCHAR   NOT NULL,
-    CONSTRAINT pk_Title PRIMARY KEY (
-        Title_ID
-     )
-);
 
 
